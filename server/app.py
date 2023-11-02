d822f5de66a0825e05597b44bc24b835abb7dd50
FridaGathima <frida.gathima@student.moringaschool.com>
2023-11-02 19:53:27 +0300

    routes

--- server/app.py
+++ server/app.py
@@ -1,36 +1,75 @@
-from flask import Flask, jsonify, request, make_response
-from flask_restful import Resource, Api
-
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api, Resource, fields, marshal_with
+from Models import db, Admin, Book, User, Cart, CartItem, Purchase, ReturnRequest
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['JWT_SECRET_KEY'] = 'a&Tn7Rc`,6^8R/<Xqg0w"u[:S\9OIbk5' 
+migrate = Migrate(app, db)
+db.init_app(app)
 api = Api(app)
+jwt = JWTManager(app)
 
+# Define resource fields for marshaling
+admin_fields = {
+    'AdminID': fields.Integer,
+    'Username': fields.String,
+    'Password': fields.String,
+    'Email': fields.String,
+    'Full_Name': fields.String,
+}
 
-class Home(Resource):
+book_fields = {
+    'BookID': fields.Integer,
+    'Title': fields.String,
+    'Author': fields.String,
+    'Genre': fields.String,
+    'Description': fields.String,
+    'Price': fields.Float
+}
+
+# Define resource classes for each model
+class AdminResource(Resource):
+    @marshal_with(admin_fields)
+    def post(self):
+        data = request.form
+        new_admin = Admin(Username=data['Username'], Password=data['Password'], Email=data['Email'], Full_Name=data['Full_Name'])
+        db.session.add(new_admin)
+        db.session.commit()
+        return new_admin
+
+    @marshal_with(admin_fields)
     def get(self):
-        return ('Welcome to LitHaven Api. The comprehensive API for Booklovers')
-    
+        admins = Admin.query.all()
+        return admins
 
-class BooksList(Resource):
-    # to retrieve all books
-    def get(self):
-        # return books
-        pass
+    @marshal_with(admin_fields)
+    def put(self, id):
+        admin = Admin.query.get(id)
+        if admin is not None:
+            data = request.form
+            admin.Username = data['Username']
+            admin.Password = data['Password']
+            admin.Email = data['Email']
+            admin.Full_Name = data['Full_Name']
+            db.session.commit()
+            return admin
 
-class BooksById(Resource):
-    # to retrieve books by id
-    def get(self, book_id):
-        # return books[book_id: books[book_id]]
-        pass
+    def delete(self, id):
+        admin = Admin.query.get(id)
+        if admin is not None:
+            db.session.delete(admin)
+            db.session.commit()
+            return {'message': 'Admin deleted successfully'}, 204
 
 
-
-    
-    
-
-api.add_resource(Home, '/')
+api.add_resource(AdminResource, '/admins', '/admins/<int:id>')
+api.add_resource(BookResource, '/books', '/books/<int:id>')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == '_main_':
+    app.debug = True
+    app.run(debug=False)
---
+++ server/secrets.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+# Define the characters that can be used in the secret key
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate a secure random string for the secret key
+secret_key = ''.join(random.SystemRandom().choice(characters) for _ in range(32))
+
+# Print the secret key
+print(secret_key)
