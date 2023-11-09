ee8850b092d38cd29d95a00e39b585814488e825
FridaGathima <frida.gathima@student.moringaschool.com>
2023-11-09 19:57:32 +0300

    removed database.db

--- server/swagger.py
+++ server/swagger.py
@@ -1,26 +1,26 @@
-from flask import Flask
-from flask_restful import Api, Resource
-from flasgger import Swagger
+# from flask import Flask
+# from flask_restful import Api, Resource
+# from flasgger import Swagger
 
-app = Flask(__name__)
-api = Api(app)
-swagger = Swagger(app)
+# app = Flask(__name__)
+# api = Api(app)
+# swagger = Swagger(app)
 
-# ... (rest of the code remains the same)
+# # ... (rest of the code remains the same)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        """
-        This endpoint returns a simple 'Hello, World!' message.
-        ---
-        responses:
-          200:
-            description: A simple 'Hello, World!' message.
-        """
-        return {'message': 'Hello, World!'}
+# class HelloWorld(Resource):
+#     def get(self):
+#         """
+#         This endpoint returns a simple 'Hello, World!' message.
+#         ---
+#         responses:
+#           200:
+#             description: A simple 'Hello, World!' message.
+#         """
+#         return {'message': 'Hello, World!'}
 
-api.add_resource(HelloWorld, '/')
+# api.add_resource(HelloWorld, '/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
